7aa94fa0a88e33a47c70c56d78c36e4f130090ce
Nick Valdez <nicholastvaldez@gmail.com>
2024-05-28 21:05:36 -0500

    create check amount text field

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Nicholas Valdez on 5/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPerctages = [10, 15, 20, 25, 0]
+        
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
